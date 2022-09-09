8af233713392d3d80ca86d93e67a1ba65f0a4e61
Todd <Todd_brown1@baylor.edu>
2022-09-08 20:07:27 -0500

    I think im good?

--- avg_steps.py
+++ avg_steps.py
@@ -3,9 +3,9 @@ import csv
 infile = open('steps.csv','r')
 outfile= open('avg_steps.csv', 'w')
 
-Every_month = ['Discard','January','Febuary','March','April','May','June','July','August','September','October','November','December']
+every_month = ['Discard','January','Febuary','March','April','May','June','July','August','September','October','November','December']
 csv_file = csv.reader(infile,delimiter=",")
-next(csv_file)
+infile.readline()
 month = 1
 day = 0 
 total_steps = 0
@@ -16,11 +16,11 @@ for line in csv_file:
     
     else:
         avg_steps = format(total_steps/day,'.2f')
-        outfile.write(month[int(Every_month)] + ',' + str(avg_steps) + '\n')
+        outfile.write(every_month[int(month)] + ',' + str(avg_steps) + '\n')
         month = line[0]
         total_steps=int(line[1])
         day = 1
 
 avg_steps = format(total_steps/day, '.2f')
-outfile.write(month[int(Every_month)] + ',' + str(avg_steps) + '\n')
+outfile.write(every_month[int(month)] + ',' + str(avg_steps) + '\n')
 outfile.close()
